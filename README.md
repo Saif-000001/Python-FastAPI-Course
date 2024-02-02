85b8b89bf5be4fbec36ddb16754ed50ce5480464
Saif <dev.mdsaifulislam@gmail.com>
2024-02-02 12:32:49 +0600

    path parameter

--- README.md
+++ README.md
@@ -12,9 +12,8 @@
 #---------------> Deployment
 01. Ubuntu
 02. Heroku
-
-# Docker
-# CI/CD
+03. Docker
+04. CI/CD
 # FastApi
 # SQLAlchemy
 
@@ -29,5 +28,49 @@ install ---> python3 -m venv venv
 
 # FastApi Run 
 -> uvicorn main:app --reload
+
+--------------------------02-02-2024-------------------
+# create a path operation
+# HTTP Methods 
+<!--    POST: to create data.
+        GET: to read data.
+        PUT: to update data.
+        DELETE: to delete data 
+
+        # More exotic ones
+           # OPTIONS
+           # HEAD
+           # PATCH
+           # TRACE
+-->
+
+# path operation function
+<!-- 
+       # path: is /.
+       # operation: is get.
+       # function: is the function below the "decorator" (below @app.get("/"))
+
+       Example: @app.get("/")
+ -->
+
+# Recap
+<!-- 
+    Import FastAPI.
+    Create an app instance.
+    Write a path operation decorator (like @app.get("/")).
+    Write a path operation function (like def root(): ... above).
+    Run the development server (like uvicorn main:app --reload). 
+-->
+
+# You can only use await inside of functions created with async def.
+
+# Order matters
+<!-- if tow function is same pathe, fist function always show in documentation  -->
+
+
+# Create an Enum class
+<!-- 
+        Enumerations in Python are implemented by using the module named “enum“. Enumerations are created using classes. Enums have names and values associated with them 
+-->
 
 
--- main.py
+++ main.py
@@ -1,7 +1,65 @@
 from fastapi import FastAPI
+from enum import Enum
+
+# class ModelName(str, Enum):
+#     pujaCherry = 'pujaCherry'
+#     prarthanaFardinDighi = 'prarthanaFardinDighi'
+#     purnima = 'purnima'
+#     shabnur = "shabnur"
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"data":" Hello World"}
+""" 
+@app.get('/models/{model_name}')
+def select_model(model_name:ModelName):
+    if model_name == 'pujaCherry':
+        return f"{model_name} is very young model"
+    elif model_name == 'prarthanaFardinDighi':
+        return f"{model_name} is look like good"
+    elif model_name == 'purnima':
+        return f"{model_name} is crush of every generation"
+    else:
+        return f"{model_name} is just selected model" 
+"""
+
+# @app.get('/models/{model_name}')
+# def get_model(model_name:ModelName):
+#     if model_name is ModelName.prarthanaFardinDighi:
+#         return f"{model_name} is good look model"
+#     elif model_name == ModelName.pujaCherry:
+#         return f"{model_name} is very young model"
+    
+#     return f"{model_name} is old model"
+
+
+
+    
+
+@app.get('/')
+def read():
+    return {"message": "Hello World"}
+
+# Path parameters with types
+""" 
+@app.get('/iteams/{iteams_id}')
+def get_read(iteams_id:int):
+    return{'id': iteams_id} 
+"""
+
+
+# Order matters
+""" 
+@app.get('/iteams/me')
+def get():
+    return{'name': 'MSI'}
+
+@app.get('/iteams/me')
+def get_hello():
+    return{'name': 'SAIF'} 
+"""
+
+# Predefined values
+
+
+
+
